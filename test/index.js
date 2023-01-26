19e852033d433bc533acb11c1e9776daad3d833c
Axel Rothe <a.rothe@vanrothe.com>
2023-01-26 21:09:37 +0100

    adds test, corrects typescript types

---
+++ test/index.js
@@ -0,0 +1,42 @@
+import { DeepJS } from '../build/index.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { DEEPVA_API_KEY } = process.env
+
+console.log(process.env)
+
+const deep = new DeepJS({
+    apiKey: process.env.DEEPVA_API_KEY,
+});
+
+deep.checkAuthorization().then((response) => {
+    console.log(response);
+
+    const sources = [
+        "https://demo.deepva.com/demo1.jpg"
+    ]
+
+    const modules = [
+        DeepJS.modules.object_scene_recognition.build({
+            model: 'general-c',
+            min_confidence: 50,
+            language: 1
+        })
+    ]
+
+    deep.createJob(sources, modules).then((response) => {
+        console.log("Job created : " + response.id);
+
+        deep.waitForJob(response.id).then((response) => {
+            console.log(response);
+        })
+
+    }).catch((error) => {
+        console.log(error);
+    })
+
+}).catch((error) => {
+    console.log(error);
+});
+
